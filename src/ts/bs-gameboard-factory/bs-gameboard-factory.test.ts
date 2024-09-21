3eee607142d71ba29df957657656764debd334a1
JOHN KEYS CLOUD <90482169+JohnKeysCloud@users.noreply.github.com>
2024-09-21 14:40:39 -0400

    chore(get-valid-positions.test.ts): move ship placement validation test
    - Relocate the ship position validation test from
    `bs-gameboard-factory.test.ts` to `get-valid-positions.test.ts` for
    better test organization.
    - Ensure separation of concerns by isolating the
    `getValidShipPositions` test.

--- src/ts/bs-gameboard-factory/bs-gameboard-factory.test.ts
+++ src/ts/bs-gameboard-factory/bs-gameboard-factory.test.ts
@@ -1,8 +1,5 @@
 import { BattleshipBoardFactory } from './bs-gameboard-factory';
-import { IValidPlacementWrapperParams } from './bs-gameboard-factory';
-import { IValidPositionsResult } from './bs-gameboard-factory';
-import { IPosition } from './helpers/get-valid-positions/get-valid-positions';
-import type { Coordinates } from './bs-gameboard-factory';
+
 describe('`BattleshipBoardFactory` Instance Board Creation', () => {
   test('board creation/retrieval: should match emptyBoard: %o', () => {
     let testBoard: BattleshipBoardFactory = new BattleshipBoardFactory();
@@ -21,98 +18,6 @@ describe('`BattleshipBoardFactory` Instance Board Creation', () => {
     // Perform the test
     expect(serializeBoard(testBoard.board)).toBe(serializeBoard(emptyBoard));
   });
-});
-
-describe('`validateShipPlacement`', () => {
-  // Initialized with instance to ensure definition in setup/helper functions
-  let testBoard: BattleshipBoardFactory = new BattleshipBoardFactory();
-
-  // Creates a new instance for each test run
-  beforeEach(() => {
-    testBoard = new BattleshipBoardFactory();
-  });
-
-  interface validPositionsTestCase {
-    shipConfigs: IValidPlacementWrapperParams;
-    validPositions: IValidPositionsResult;
-  }
-
-  // Test function utility
-  const getValidPositionsArray = (input: IValidPlacementWrapperParams) =>
-    testBoard.getValidShipPositions(input);
-
-  // Ensure interface compliance
-  const createPlacementParams = (
-    direction: 'horizontal' | 'vertical',
-    gamePieceSize: number
-  ): IValidPlacementWrapperParams => ({
-    direction,
-    gamePieceSize,
-  });
-
-  // Dynamically create expected test result
-  function createTestCaseResult(
-    direction: 'horizontal' | 'vertical',
-    gamePieceSize: number
-  ): IValidPositionsResult {
-    const createPosition = (
-      bow: Coordinates,
-      stern: Coordinates
-    ): IPosition => {
-      return { bow, stern };
-    };
-
-    // Initalizes return object to contain all valid positions
-    const validPositions: IValidPositionsResult = {};
-
-    // Creates row/column objects
-    for (let i = 0; i < testBoard.boardSize; i++) {
-      const axisTemplate =
-        direction === 'horizontal' ? `row-${i}` : `column-${i}`;
-
-      // Initializes array for row/column
-      validPositions[axisTemplate] = [];
-
-      // Populates each array
-      for (let j = 0; j + (gamePieceSize - 1) < testBoard.boardSize; j++) {
-        let position: IPosition =
-          direction === 'horizontal'
-            ? createPosition([i, j], [i, j + gamePieceSize - 1])
-            : createPosition([j, i], [j + gamePieceSize - 1, i]);
-
-        validPositions[axisTemplate].push(position);
-      }
-    }
-
-    return validPositions;
-  }
-
-  // Actual Test Object
-  const testCases: validPositionsTestCase[] = [
-    {
-      shipConfigs: createPlacementParams('horizontal', 5),
-      validPositions: createTestCaseResult('horizontal', 5),
-    },
-    {
-      shipConfigs: createPlacementParams('vertical', 2),
-      validPositions: createTestCaseResult('vertical', 2),
-    },
-    {
-      shipConfigs: createPlacementParams('horizontal', 4),
-      validPositions: createTestCaseResult('horizontal', 4),
-    },
-    {
-      shipConfigs: createPlacementParams('vertical', 3),
-      validPositions: createTestCaseResult('vertical', 3),
-    },
-  ];
-
-  test.each(testCases)(
-    'valid position retrieval (empty board): %o',
-    ({ shipConfigs, validPositions }) => {
-      expect(getValidPositionsArray(shipConfigs)).toEqual(validPositions);
-    }
-  );
 });
 
 // 💭 --------------------------------------------------------------
