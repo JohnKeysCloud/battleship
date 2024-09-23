4022d79e5d9a25663131da773836796c75480691
JOHN KEYS CLOUD <90482169+JohnKeysCloud@users.noreply.github.com>
2024-09-23 14:06:15 -0400

    feat(utilities): add `getRandomInt` function - returns a random integer

--- src/ts/utilities/random-utilities.ts
+++ src/ts/utilities/random-utilities.ts
@@ -1,1 +1,8 @@
-export const getRandomInt = (max: number) => Math.floor(Math.random() * max);
+export const getRandomInt = (min: number, max: number): number =>
+  Math.floor(Math.random() * (max - min + 1)) + min;
+
+export const areArraysEqual = <T>(arrayOne: T[], arrayTwo: T[]) =>
+  arrayOne.length === arrayTwo.length
+  &&
+  arrayOne.every((value, index) => value === arrayTwo[index]);
+  
