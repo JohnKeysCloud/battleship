8d7f09b192ec6e071c27ebce44ecb6046ae206b4
JOHN KEYS CLOUD <90482169+JohnKeysCloud@users.noreply.github.com>
2024-11-07 01:23:56 -0500

    feat(random-utilities.ts): add `getRandomInt` function
    - Accepts `min` and `max` parameters (both inclusive)
    - Returns a random integer within the specified range

--- src/typescript/utilities/random-utilities.ts
+++ src/typescript/utilities/random-utilities.ts
@@ -1,12 +1,19 @@
-export const getRandomInt = (min: number, max: number): number =>
-  Math.floor(Math.random() * (max - min + 1)) + min;
-
 export const areArraysEqual = <T>(arrayOne: T[], arrayTwo: T[]) =>
   arrayOne.length === arrayTwo.length
   &&
   arrayOne.every((value, index) => value === arrayTwo[index]);
 
+export const getRandomInt = (min: number, max: number): number =>
+  Math.floor(Math.random() * (max - min + 1)) + min;
+
+export const isEmptyObject = (obj: Record<PropertyKey, unknown>): boolean => {
+  return (
+    Object.keys(obj).length === 0 &&
+    Object.getOwnPropertySymbols(obj).length === 0
+  );
+}
+
 export type Range<N extends number, Acc extends number[] = []> =
   Acc['length'] extends N
     ? Acc[number]
-    : Range<N, [...Acc, Acc['length']]>; 
+    : Range<N, [...Acc, Acc['length']]>;
