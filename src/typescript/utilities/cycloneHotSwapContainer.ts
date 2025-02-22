53c3b9ddb06b240386d140af3e41965b9ee7066b
JOHN KEYS CLOUD <90482169+JohnKeysCloud@users.noreply.github.com>
2025-02-22 15:04:42 -0500

    refactor(cycloneHotSwapContainer): add swap by order method
    - Renames create-hot-swap-container.ts to cycloneHotSwapContainer.ts

--- src/typescript/utilities/cycloneHotSwapContainer.ts
+++ src/typescript/utilities/cycloneHotSwapContainer.ts
@@ -3,15 +3,17 @@ import { createElement } from "./random-utilities";
 /**
  * A container for dynamically swapping and managing fragments of the DOM.
  * 
- * @class HotSwapContainer
+ * @class CycloneHotSwapContainer
  * @param {keyof HTMLElementTagNameMap} elementTag - The HTML element tag for the container (e.g., 'section', 'div').
  * @param {{ [key: string]: DocumentFragment }} fragments - A collection of swappable fragments, keyed by string identifiers.
  * @param {string} initialFragmentKey - The key of the initial fragment to render in the container.
  * @param {string[]} [classNames=[]] - An array of class names to apply to the container element (optional).
  * @param {Record<string, string>} [attributes={}] - A set of attributes to apply to the container element (optional).
  */
-export class HotSwapContainer {
+export class CycloneHotSwapContainer {
   private readonly hotSwapContainer: HTMLElement;
+  private readonly fragmentsArray: DocumentFragment[];
+  #currentIndex: number;
 
   constructor(
     private readonly elementTag: keyof HTMLElementTagNameMap,
@@ -32,6 +34,14 @@ export class HotSwapContainer {
       this.attributes
     );
 
+    this.fragmentsArray = Object.values(this.fragments);
+
+    this.#currentIndex = this.setInitialIndex(
+      this.fragmentsArray,
+      this.fragments,
+      this.initialFragmentKey
+    );
+
     this.hotSwapContainer.appendChild(this.fragments[this.initialFragmentKey]);
   }
 
@@ -39,20 +49,40 @@ export class HotSwapContainer {
     targetElement.appendChild(this.hotSwapContainer);
   }
 
-  public replaceFragment(fragment: DocumentFragment) {
-    this.hotSwapContainer.replaceChildren(fragment);
-  }
-
-  public replaceFragmentByKey(key:string): void {
+  public swapFragmentByKey(key: string): void {
     const fragment = this.getFragment(key);
-    this.replaceFragment(fragment);
+    this.swapFragment(fragment);
+    this.#currentIndex = this.fragmentsArray.indexOf(fragment);
   }
 
-  private getFragment(key:string): DocumentFragment {
+  public swapByOrder(): void {
+    this.#currentIndex = (this.#currentIndex + 1) % this.fragmentsArray.length;
+    this.swapFragment(this.fragmentsArray[this.#currentIndex]);
+  }
+
+  private setInitialIndex(
+    fragmentsArray: DocumentFragment[],
+    fragments: { [key: string]: DocumentFragment },
+    initialFragmentKey: string
+  ): number {
+    const initialIndex = fragmentsArray.indexOf(fragments[initialFragmentKey]);
+    if (initialIndex === -1) {
+      throw new Error(
+        `Initial fragment with key "${initialFragmentKey}" does not exist.`
+      );
+    }
+    return initialIndex;
+  }
+
+  private getFragment(key: string): DocumentFragment {
     const fragment = this.fragments[key];
     if (!fragment) {
       throw new Error(`Fragment with key "${key}" does not exist.`);
     }
     return fragment;
+  }
+
+  private swapFragment(fragment: DocumentFragment) {
+    this.hotSwapContainer.replaceChildren(fragment);
   }
 }
